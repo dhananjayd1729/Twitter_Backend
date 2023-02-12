ca7db271e6dfef869400e5b51f6bc1374f4a07bf
Dhananjay <dhananjay@pop-os.localdomain>
2023-02-12 19:27:17 +0530

    Added controller & routes for post tweet api

---
+++ src/controllers/tweet-controller.js
@@ -0,0 +1,23 @@
+import TweetService from "../services/tweet-service.js";
+
+const tweetService = new TweetService();
+
+export const createTweet = async (req, res) => {
+  try {
+    const response = await tweetService.create(req.body);
+    console.log(response);
+    return res.status(201).json({
+      success: true,
+      data: response,
+      message: "Successfully created a tweet",
+      error: {},
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      data: {},
+      message: "Something went wrong",
+      err: error,
+    });
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import express from "express";
 import connect from "./config/database.js";
+
+import bodyParser from "body-parser";
+import apiRoutes from "./routes/index.js";
+
 const app = express();
 
-import TweetService from "./services/tweet-service.js";
-
 const startAndPrepareServer = async () => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use("/api", apiRoutes);
+
   app.listen(3000, async () => {
     console.log(`Server started `);
     await connect();
     console.log("Mongo DB connected");
-
-    // const service = new TweetService();
-    // const tweet = await service.create({
-    //   content: "New #TWeetstf #TES$TING #WITHCAPS",
-    // });
-
-    // console.log(tweet);
   });
 };
 
